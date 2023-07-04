148acf7b4fe216a1843bebe527d81fbcafaf9878
Kunal Nayyar <48790070+kunal2002@users.noreply.github.com>
2023-07-04 09:35:26 +0530

    Create ConstructLexicographicallyLargestSeqLC.cpp

---
+++ ConstructLexicographicallyLargestSeqLC.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool backTrack(vector<int>& vec, int p, int n, vector<bool>& vis, int sz)
+    {
+        if(p == sz)
+        {
+            if(count(vec.begin(), vec.end(), 0) == 0)
+                return true;
+            return false;
+        }
+        for(int i = n; i >= 1; i--)
+        {
+            if(i == 1 && vec[p] == 0 && vis[i] == false)
+            {
+                vis[i] = true;
+                vec[p] = i;
+                if(backTrack(vec, p + 1, n, vis, sz))
+                    return true;
+                vis[i] = false;
+                vec[p] = 0;
+            }
+            else if(p + i < sz && vec[p] == 0 && vec[p + i] == 0 && vis[i] == false)
+            {
+                vis[i] = true;
+                vec[p] = vec[p + i] = i;
+                if(backTrack(vec, p + 1, n, vis, sz))
+                    return true;
+                vis[i] = false;
+                vec[p] = vec[p + i] = 0;
+            }
+        }
+        if(count(vec.begin(), vec.end(), 0) == 0)
+            return true;
+        return backTrack(vec, p + 1, n, vis, sz);
+    }
+    vector<int> constructDistancedSequence(int n) {
+        int size = 2 * n - 1;
+        vector<int> vec(size, 0);
+        vector<bool> vis(n + 1, false);
+        bool what = backTrack(vec, 0, n, vis, size);
+        return vec;
+    }
+};
