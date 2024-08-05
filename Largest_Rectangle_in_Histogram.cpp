3913f5ebed9f29cb1d829f40dd596e1b9edd91cc
Kunal Nayyar <48790070+kunal2002@users.noreply.github.com>
2024-08-05 20:08:52 +0530

    Create Largest_Rectangle_in_Histogram.cpp

---
+++ Largest_Rectangle_in_Histogram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int maxArea = 0;
+        for(int i = 0; i < heights.size(); i++)
+        {
+            while(!st.empty() && heights[st.top()] > heights[i])
+            {
+                int ele = heights[st.top()];
+                st.pop();
+                int nse = i, pse = st.empty() ? -1 : st.top();
+                maxArea = max(maxArea, ele * (nse - pse - 1));
+            }
+            st.push(i); 
+        }
+        while(!st.empty())
+        {
+            int ele = heights[st.top()];
+            st.pop();
+            int nse = heights.size(), pse = st.empty() ? -1 : st.top();
+            maxArea = max(maxArea, ele * (nse - pse - 1));
+        }
+        return maxArea;
+    }
+};
