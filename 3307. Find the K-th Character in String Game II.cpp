0b393ad9cce758c8081a7d02cb264ea21d095ec1
Kunal Nayyar <48790070+kunal2002@users.noreply.github.com>
2025-07-04 22:59:24 +0530

    Create 3307. Find the K-th Character in String Game II.cpp

---
+++ 3307. Find the K-th Character in String Game II.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    char kthCharacter(long long k, vector<int>& operations) {
+        if(k == 1)
+            return 'a';
+        int n = operations.size();
+        long long len = 1;
+        long long newK = 0;
+        int operationType = -1;
+        for(int i = 0; i < n; i++)
+        {
+            len <<= 1;
+            if(len >= k)
+            {
+                newK = k - (len / 2);
+                operationType = operations[i];
+                break;
+            }
+        }
+        char ch = kthCharacter(newK, operations);
+        if(operationType == 0)
+            return ch;
+        return (ch == 'z' ? 'a' : ch + 1);
+    }
+};
