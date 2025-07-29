8ab5d7bc761da19b94c14f503f32c1e1ba1347ba
Kunal Nayyar <48790070+kunal2002@users.noreply.github.com>
2025-07-29 12:25:02 +0530

    Create 3629. Minimum Jumps to Reach End via Prime Teleportation LC.cpp

---
+++ 3629. Minimum Jumps to Reach End via Prime Teleportation LC.cpp
@@ -0,0 +1,62 @@
+vector<bool> isPrime(1e6 + 1, true);
+class Solution {
+public:
+        void fill()
+        {
+            isPrime[0] = isPrime[1] = false;
+            for(int i = 2; i * i <= 1e6; i++)
+            {
+                if(isPrime[i])
+                {
+                    for(int j = i * i; j <= 1e6; j += i)
+                        isPrime[j] = false;
+                }
+            }
+        }
+    int minJumps(vector<int>& nums) {
+        if(isPrime[0])
+            fill();
+        int n = nums.size(), maxi = *ranges::max_element(nums);
+        unordered_map<int, vector<int>> mp;
+        for(int i = 0; i < n; i++)
+            mp[nums[i]].push_back(i);
+        vector<int> dist(n, -1);
+        queue<int> q;
+        q.push(0);
+        dist[0] = 0;
+        unordered_set<int> used;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            if(node - 1 >= 0 && dist[node - 1] == -1)
+            {
+                q.push(node - 1);
+                dist[node - 1] = dist[node] + 1;
+            }
+            if(node + 1 < n && dist[node + 1] == -1)
+            {
+                q.push(node + 1);
+                dist[node + 1] = dist[node] + 1;
+            }
+            if(isPrime[nums[node]] == false || used.contains(nums[node]))
+                continue;
+            for(int ele = nums[node]; ele <= maxi; ele += nums[node])
+            {
+                if(!mp.contains(ele)) 
+                    continue;
+                for(auto it : mp[ele])
+                {
+                    if(dist[it] != -1)
+                        continue;
+                    q.push(it);
+                    if(it == n - 1)
+                        return dist[node] + 1;
+                    dist[it] = dist[node] + 1;
+                }   
+            }
+            used.insert(nums[node]);
+        }
+        return dist.back();
+    }
+};
