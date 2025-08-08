923350f85ec2f814ade071620ff30ebbb3b951f3
Kunal Nayyar <48790070+kunal2002@users.noreply.github.com>
2025-08-08 19:28:59 +0530

    Update 808. Soup Servings Self Recursive Lambda LC.cpp

--- 808. Soup Servings Self Recursive Lambda LC.cpp
+++ 808. Soup Servings Self Recursive Lambda LC.cpp
@@ -6,6 +6,7 @@ public:
         if(n >= 5000)
             return 1.0;
         // CPRB (capture, params, return_type, body)
+        /*
         vector<vector<double>> dp(quantity + 1, vector<double>(quantity + 1, -1.0));
         auto f = [&] (auto&& self, int a, int b) -> double {
             if(a <= 0 && b <= 0)
@@ -18,6 +19,18 @@ public:
             if(memo >= 0.0)
                 return memo;
             return dp[a][b] = 0.25 * (self(self, a - 4, b) + self(self, a - 3, b - 1) + self(self, a - 2, b - 2) + self(self, a - 1, b - 3));
+        }; */
+        function<double(int,int)> f = [&] (int a, int b) -> double {
+            if(a <= 0 && b <= 0)
+                return 0.5;
+            if(a <= 0)
+                return 1.0;
+            if(b <= 0)
+                return 0.0;
+            double &memo = dp[a][b];
+            if(memo >= 0.0)
+                return memo;
+            return dp[a][b] = 0.25 * (f(a - 4, b) + f(a - 3, b - 1) + f(a - 2, b - 2) + f(a - 1, b - 3));
         };
         return f(f, quantity, quantity);
     }
